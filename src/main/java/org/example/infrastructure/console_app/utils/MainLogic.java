8d94225fcd5aeb7ae2f37104e46b9aefcf695122
pikafor <DAUNBLAT228@yandex.ru>
2023-11-21 05:16:26 +0300

    commit

--- src/main/java/org/example/infrastructure/console_app/utils/MainLogic.java
+++ src/main/java/org/example/infrastructure/console_app/utils/MainLogic.java
@@ -5,15 +5,34 @@ import org.example.infrastructure.console_app.mapper.UserEntityMapper;
 import org.example.infrastructure.console_app.model.UserEntity;
 import org.example.infrastructure.console_app.repositories.UserEntityRepository;
 
+import java.util.Scanner;
+
 public class MainLogic implements UserEntityRepository{
 
     public void process() {
-        UserEntity userEntity = new UserEntity(1, "Ivan", "1488");
+        String logIn, password;
+
+        sendMessage("Введите логин: ");
+        logIn = setAnswer();
+        sendMessage("Введите пароль: ");
+        password = setAnswer();
+
+        UserEntity userEntity = new UserEntity(1, logIn, password);
         System.out.println(createUser(userEntity).toString());
     }
 
     @Override
     public User createUser(UserEntity userEntity) {
         return new UserEntityMapper().createUser(userEntity);
+    }
+
+    private void sendMessage(String message) {
+        System.out.print(message);
+    }
+
+    private String setAnswer() {
+        Scanner scanner = new Scanner(System.in);
+
+        return scanner.nextLine();
     }
 }
